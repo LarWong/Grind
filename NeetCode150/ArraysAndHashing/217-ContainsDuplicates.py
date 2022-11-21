103a4027ab8d6104a883d5fac404eb81de4e9cd1
Larry <me@larrywong.dev>
2022-11-20 19:21:07 -0500

    init

---
+++ NeetCode150/ArraysAndHashing/217-ContainsDuplicates.py
@@ -0,0 +1,9 @@
+from typing import List
+
+def solution(nums: List[int]) -> bool:
+    check = set()
+    for n in nums:
+        if n in check:
+            return True
+        check.add(n)
+    return False
